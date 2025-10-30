a90212e0e0b2ea232923763e4259a4ccd48b4169
[dyad] <victordoui@gmail.com>
2025-10-30 19:25:32 -0300

    [dyad] add Dyad component tagger

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import { VitePWA } from 'vite-plugin-pwa';
+import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,7 +11,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
+  plugins: [dyadComponentTagger(), 
     react(),
     VitePWA({
       registerType: 'autoUpdate',
